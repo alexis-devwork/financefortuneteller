6d1bf645061e549973acbc216a6f88737efa1c8d
Alexis <alexis.sachin@gmail.com>
2019-08-28 16:36:27 -0400

    moving tests from app to test.py

---
+++ test.py
@@ -0,0 +1,36 @@
+from tarotdb import TarotDB
+from tweeter import Tweeter
+from reading import Reading
+from card import Card
+import time
+
+db = TarotDB("diviner.db")
+reading = Reading(db,"NYSE")
+tweet = reading.get_reading()
+tw = Tweeter("credentials.txt")
+tw.tweet(tweet)
+db.close_con()
+
+# faces = ['Ace',
+#     'Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten',
+#     'Page','Knight','Queen','King'
+#     ]
+# suits = ['Swords','Cups','Wands','Pentacles']
+#
+
+# tw = Tweeter("credentials.txt")
+# db = TarotDB("diviner.db")
+# fool = Card(db,1,0)
+# tw.tweet(fool.card)
+# for suit in suits:
+#     strings = []
+#     for face in faces:
+#         strings.append(fool.emojify(face+" of "+suit))
+#     tweet = ', '.join(strings)
+#     posn = 0
+#     # tw.tweet(fool.emojify("Two of Pentacles"))
+#     while posn<len(tweet):
+#         tw.tweet(tweet[posn:posn+150])
+#         posn+=150
+#         time.sleep(5)
+# db.close_con()
