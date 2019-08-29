486e641ea5d15ed0b231abcf566ddf32d6dc2d1c
Alexis <alexis.sachin@gmail.com>
2019-08-29 09:04:12 -0400

    switched to dotenv for env variable usage

--- test.py
+++ test.py
@@ -1,15 +1,30 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import time
+
 from card import Card
 from reading import Reading
 from tarotdb import TarotDB
 from tweeter import Tweeter
 
-db = TarotDB("diviner.db")
-reading = Reading(db,"SPY")
-tweet = reading.get_reading()
-tw = Tweeter("credentials.txt")
-tw.tweet(tweet)
-db.close_con()
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+CONSUMER_KEY = os.environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET")
+ACCESS_KEY = os.environ.get("ACCESS_KEY")
+ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
+
+# tw = Tweeter(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+# tw.tweet("test tweet")
+
+# db = TarotDB("diviner.db")
+# reading = Reading(db,"SPY")
+# tweet = reading.get_reading()
+# tw = Tweeter("credentials.txt")
+# tw.tweet(tweet)
+# db.close_con()
 
 # faces = ['Ace',
 #     'Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten',
--- tweeter.py
+++ tweeter.py
@@ -2,14 +2,11 @@ import tweepy
 
 class Tweeter:
 
-    def __init__(self,credfile):
-        filename = open(credfile,'r')
-        f = filename.readlines()
-        filename.close()
-        self.CONSUMER_KEY = f[0].strip()
-        self.CONSUMER_SECRET = f[1].strip()
-        self.ACCESS_KEY = f[2].strip()
-        self.ACCESS_SECRET = f[3].strip()
+    def __init__(self,CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET):
+        self.CONSUMER_KEY = CONSUMER_KEY.strip()
+        self.CONSUMER_SECRET = CONSUMER_SECRET.strip()
+        self.ACCESS_KEY = ACCESS_KEY.strip()
+        self.ACCESS_SECRET = ACCESS_SECRET.strip()
         self.auth = tweepy.OAuthHandler(self.CONSUMER_KEY, self.CONSUMER_SECRET)
         self.auth.set_access_token(self.ACCESS_KEY, self.ACCESS_SECRET)
         self.api = tweepy.API(self.auth)
